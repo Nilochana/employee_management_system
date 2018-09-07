8698f28cb11b1e6dbd51128e20102e4829009c70
PATIL NILOCHANA <patilnilochana@gmail.com>
2018-09-07 18:46:03 +0530

    added salary details

---
+++ salary details.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/foot.css">
+</head>
+<nav class="navbar navbar-default color fixed-top">
+    <div class="container">
+
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav ">
+                <ul class="nav navbar-nav">
+
+                        
+
+                    <li class="act">
+
+                        <a href="football.php">
+                            <i class="fas fa-home">PAYROLL</i>
+                            <span class="sr-only"></span>
+                        </a>
+                    </li>
+                        
+                    <li>
+
+                        <a href="signup.php">
+                            <span class="glyphicon glyphicon-user"></span>EMPLOYEE</a>
+                    </li>
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="leave.php">LEAVE
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="xxx.php">WORKFLOW
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                            <li>
+                                <a href="xxxx.php">xxxx</a>
+                            </li>
+                        </u1>
+                    </li>
+                    <li>
+
+                        <a href="signup.php">
+                            <span class="glyphicon glyphicon-user"></span>REPORT</a>
+                    </li>
+                    <li>
+
+                        <a href="signup.php">
+                            <span class="glyphicon glyphicon-user"></span>OTHERS</a>
+                    </li>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<br>
+<div class="container">
+
+    <form class="well form-horizontal" action="cn.php" method="POST" id="contact_form" style="border:1px solid #ccc">
+        <fieldset>
+
+            <!-- Form Name -->
+            <legend>Salary Details</legend>
+
+            <!-- Text input-->
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Employee ID</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="employee id" placeholder="Employee ID" class="form-control" type="text" >
+                            </div>
+                        </div>
+                    </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                        <label class="col-md-4 control-label">Employee Name</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="employee name" placeholder="Employee Name" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <!-- Text input-->
+
+
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Designation</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="designation" placeholder="Designation" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+                       
+
+                    <!-- Text input-->
+
+
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Department</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="department" placeholder="Department" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+</div>
+
+
+                    <!-- Text input-->
+
+<div class="col-md-6">
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Paymode</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="paymode" placeholder="paymode" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Account Number</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="account number" placeholder="Account Number" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Bank</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="bank" placeholder="Bank" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Name</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="name" placeholder="Name" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Address</label>
+                        <div class="col-md-4 inputGroupContainer">
+                            <div class="input-group">
+
+                                <input name="address" placeholder="Address" class="form-control" type="text">
+                            </div>
+                        </div>
+                    </div>
+
+
+                   
+</div>
+
+
+                    <!-- Button -->
+                    <div class="form-group">
+                        <div class=" row">
+                        <div class="col-md-3 "></div>
+                        <div class="col-md-3">
+                      
+                        <a href="signup.php" class="btn btn-success bn" style="width:170px; padding: 10px; cursor: pointer;  font-weight: bold; background:black; color: gray; border-radius: 10px; border: 1px solid black; font-size: 100%;">Reset</a>
+                     
+                           
+</div> 
+                        <div class="col-md-3">
+                        <a href="signup.php" class="btn btn-success bn" style="width:170px; padding: 10px; cursor: pointer;  font-weight: bold; background:black; color: gray; border-radius: 10px; border: 1px solid black; font-size: 100%;">Exit</a>
+</div>    
+                        </div>
+                    </div>
+            
+                
+</div>
+             </div>
+            </div> 
+        </fieldset>
+    </form>
+    </div>
+    </div>
+    <br>
+    <div>
+        </div>
+        
+
+        </body>
+
+</html>
